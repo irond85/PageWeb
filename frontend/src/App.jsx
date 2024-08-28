c1a0ad6b33ec457685f87a45d4a694fb59905bf6
IronD (Sergio) <ofocacion.34@gmail.com>
2024-08-27 22:11:01 -0500

    Se crea boton para enviar a whatsapp

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,18 @@ import MainPageView from "./views/MainPageView";
 import "./App.css";
 
 function App() {
-
   return (
     <>
       <MainPageView />
+      <div style={{ display: "none" }}>
+        <a target="_blank" href="https://icons8.com/icon/16713/whatsapp">
+          WhatsApp
+        </a>{" "}
+        icono de{" "}
+        <a target="_blank" href="https://icons8.com">
+          Icons8
+        </a>
+      </div>
     </>
   );
 }
---
+++ frontend/src/components/FloatButtonComponent.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Button, Tooltip } from "antd";
+import whatsappIcon from "../assets/whatsappIcon.png";
+
+const FloatButtonComponent = () => {
+  return (
+    <>
+      <div style={{ position: "fixed", bottom: "20px", right: "20px" }}>
+        <Tooltip title="Escribenos a nuestro Whatsapp!" placement="left">
+          <Button
+            style={{ width: "72px", height: "72px" }}
+            type="text"
+            shape="circle"
+            href="https://api.whatsapp.com/send/?phone=573177862624&text&type=phone_number&app_absent=0"
+            target="_blank"
+          >
+            <img
+              style={{ width: "100%", height: "100%", objectFit: "contain" }}
+              src={whatsappIcon}
+              alt="whatsapp icon"
+            />
+          </Button>
+        </Tooltip>
+      </div>
+      {/* <FloatButton
+        style={{ width: "32px", height: "32px" }}
+        icon={<WhatsAppOutlined />}
+        href="https://api.whatsapp.com/send/?phone=573177862624&text&type=phone_number&app_absent=0"
+        target="_blank"
+        tooltip={<div>Escribenos a nuestro Whatsapp!</div>}
+      /> */}
+    </>
+  );
+};
+
+export default FloatButtonComponent;
