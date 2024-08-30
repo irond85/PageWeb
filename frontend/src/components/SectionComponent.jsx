3ae2da8c946c96978bccd33a40b47bdcb46e6c82
IronD (Sergio) <ofocacion.34@gmail.com>
2024-08-29 21:26:34 -0500

    Ajuste section component y main page view

--- frontend/src/components/SectionComponent.jsx
+++ frontend/src/components/SectionComponent.jsx
@@ -1,8 +1,6 @@
-import React from "react";
+import { React, children } from "react";
 import { Layout } from "antd";
-import CardComponent from "../components/CardComponent.jsx";
 import "../App.css";
-import reactSvg from "../assets/react.svg";
 
 const { Content } = Layout;
 const styleRows = {
@@ -11,29 +9,23 @@ const styleRows = {
   margin: "0 5%",
 };
 
-const SectionComponent = ({ heightSection, titleSection }) => {
+const SectionComponent = ({
+  children,
+  colorBackground = "#f5f5f5",
+  heightSection,
+  titleSection,
+}) => {
   return (
-    <Layout>
+    <Layout style={{ background: colorBackground }}>
       <Content
         style={{
           height: heightSection,
-          margin: "5px 0",
+          margin: "55px 0",
         }}
       >
         <div className="row" style={styleRows}>
-          <h2 style={{ textAlign: "center" }}>{titleSection}</h2>
-        </div>
-        <div className="row" style={styleRows}>
-          <CardComponent
-            srcImgCard={reactSvg}
-            titleCard={"Electricidad"}
-            descriptionCard={"Esta es la descripcion de electricidad"}
-          />
-          <CardComponent
-            srcImgCard={reactSvg}
-            titleCard={"Electricidad"}
-            descriptionCard={"Esta es la descripcion de electricidad"}
-          />
+          <h1 style={{ textAlign: "center" }}>{titleSection}</h1>
+          {children}
         </div>
       </Content>
     </Layout>
--- frontend/src/views/MainPageView.jsx
+++ frontend/src/views/MainPageView.jsx
@@ -4,6 +4,11 @@ import HeaderComponent from "../components/HeaderComponent.jsx";
 import CarouselComponent from "../components/CarouselComponent.jsx";
 import SectionComponent from "../components/SectionComponent.jsx";
 import FloatButtonComponent from "../components/FloatButtonComponent.jsx";
+import CardComponent from "../components/CardComponent.jsx";
+import { Button, Tooltip } from "antd";
+import reactSvg from "../assets/react.svg";
+import whatsappIcon from "../assets/whatsappIcon.png";
+
 const { Title } = Typography;
 
 function MainPageView() {
@@ -11,6 +16,38 @@ function MainPageView() {
     <>
       <HeaderComponent />
       <CarouselComponent />
+      <SectionComponent
+        colorBackground="#F79F18"
+        heightSection="100%"
+        titleSection="SOLICITA UN SERVICIO"
+      >
+        <div
+          className="col-5 col-sm-4 col-md-4 col-lg-3 col-xl-2 col-xxl-6"
+          style={{ display: "flex", justifyContent: "center" }}
+        >
+          <p>
+            ¿Tienes un problema y necesitas una solución rápida? Comunícate con
+            nosotros a través de nuestro WhatsApp.
+          </p>
+        </div>
+        <div
+          className="col-5 col-sm-4 col-md-4 col-lg-3 col-xl-2 col-xxl-3"
+          style={{ display: "flex", alignItems: "center" }}
+        >
+          <Button
+            type="primary"
+            shape="round"
+            style={{ background: "#fff", color: "#000" }}
+          >
+            <img
+              style={{ width: "100%", height: "100%", objectFit: "contain" }}
+              src={whatsappIcon}
+              alt="whatsapp icon"
+            />
+            Clic aquí!
+          </Button>
+        </div>
+      </SectionComponent>
       <div id="services">
         <Title
           level={1}
@@ -21,12 +58,29 @@ function MainPageView() {
           }}
           id="services"
         >
-          Servicios
+          Nuestros Servicios
         </Title>
         <ServicesView />
       </div>
       <br />
-      <SectionComponent heightSection='450px' titleSection='Servicios Destacados'/>
+      <SectionComponent
+        heightSection="100%"
+        titleSection="Servicios Destacados"
+      >
+        <CardComponent
+          srcImgCard={reactSvg}
+          titleCard={"Instalación y Adecuación de neveras"}
+          descriptionCard={
+            "Esta es la descripcion de Instalación y Adecuación de neveras."
+          }
+        />
+        <CardComponent
+          srcImgCard={reactSvg}
+          titleCard={"Instalación de Gas"}
+          descriptionCard={"Esta es la descripcion de Instalación de Gas."}
+        />
+      </SectionComponent>
+      <br />
       <FloatButtonComponent />
     </>
   );
