f5be0f4bbb8c8e1abbdb61aa349295d039ad0d52
irond85 <69101982+irond85@users.noreply.github.com>
2024-11-02 10:01:42 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://http://plomerosyconstructoressas.com",
+  base: "http://http://plomerosyconstructoressas.com",
 });
